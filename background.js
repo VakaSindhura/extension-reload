e937ea718277ecd31b5d183ca511c1c003aa4e4a
VakaSindhura <vakasindhura@gmail.com>
2019-04-13 16:27:11 +0530

    Reloads extensiona nd current tab

--- background.js
+++ background.js
@@ -21,4 +21,18 @@ chrome.contextMenus.onClicked.addListener(function sendfunc(tab) {
   }, function (tabs) {
     chrome.tabs.reload(tabs[0].id);
   });
+
+  chrome.tabs.query({url: "chrome://extensions/"}, function(tab) {
+    chrome.tabs.reload(tab[0].id);
+  }); 
 });
+
+// const socket = new WebSocket('ws://localhost:3000/');
+// // Connection opened
+// socket.addEventListener('open', function (event) {
+//   socket.send(`Hello server I'm extension 😘`);
+// });
+// // on message from server
+// socket.addEventListener('message', (event) => {
+//   alert(event.data);
+// });
