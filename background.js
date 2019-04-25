9e6073764b5fb5169083f97e3490541450d86951
VakaSindhura <vakasindhura@gmail.com>
2019-04-25 18:44:38 +0530

    small changes made

--- background.js
+++ background.js
@@ -10,32 +10,25 @@ chrome.contextMenus.onClicked.addListener(function sendfunc(tab) {
     var ext ={};
     console.log('extensions are', a);
 
-    for(var i=0;i<=a.length;i++){
+    for(var i=1;i<=a.length;i++){
       ext=a[i];
-      if((ext.enabled === true) && (ext.installType === "development") 
-          && (ext.name!=="Reloads current tab and extension page")) {
+      if(ext.enabled && (ext.installType === "development") 
+          && (ext.name!=="Reloads current tab and custom extensions")) {
         console.log('If', ext.name);
         chrome.management.setEnabled(ext.id, false);
         chrome.management.setEnabled(ext.id, true);
       }
     }
   });
-  
+
   chrome.tabs.query({
     active: true,
     currentWindow: true
-  }, function (tabs) {
-    chrome.tabs.reload(tabs[0].id);
+  }, (tabs) => {
+    if(tabs.length) {
+      chrome.tabs.reload(tabs[0].id);
+    }
   });
 
 });
 
-// const socket = new WebSocket('ws://localhost:3000/');
-// // Connection opened
-// socket.addEventListener('open', function (event) {
-//   socket.send(`Hello server I'm extension 😘`);
-// });
-// // on message from server
-// socket.addEventListener('message', (event) => {
-//   alert(event.data);
-// });
