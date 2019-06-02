961c33fff296a77c4a444e6f4f855892d919be60
VakaSindhura <vakasindhura@gmail.com>
2019-06-02 21:24:57 +0530

    Added styles to page

--- background.js
+++ background.js
@@ -1,24 +1,31 @@
-chrome.contextMenus.create({
-  id: "Roload Reload",
-  title: "Reload Extension",
-  contexts: ["page"],
-});
+// chrome.contextMenus.create({
+//   id: "Roload Reload",
+//   title: "Reload unpacked Extension",
+//   contexts: ["page"],
+// });
 
-chrome.contextMenus.onClicked.addListener(function sendfunc(tab) {
-
-  chrome.management.getAll(function (a) {
+function reloadAll() {
+  //Gets all the extension list from extensions tab
+  chrome.management.getAll(function reload(a) {
+    var StoredExtensions = JSON.parse(localStorage.getItem('storedExtensions'));
     var ext = {};
-
+    var isCurrentExtensionStored = false;
     for (var i = 0; i <= a.length - 1; i++) {
       ext = a[i];
-      if (ext.enabled && (ext.installType === "development")
-        && !(ext.description && ext.description.includes("Reloads Active"))) {
+      const {name, id} = ext;
+      isCurrentExtensionStored = checkForExtension({id,name}, StoredExtensions);
+      if (
+        isCurrentExtensionStored &&   
+        !(ext.description && ext.description.includes("Reloads Active")) &&
+        (!ext.isApp)
+        ) {
         chrome.management.setEnabled(ext.id, false);
         chrome.management.setEnabled(ext.id, true);
       }
     }
   });
 
+  // Reloads the current tab
   chrome.tabs.query({
     active: true,
     currentWindow: true
@@ -27,5 +34,30 @@ chrome.contextMenus.onClicked.addListener(function sendfunc(tab) {
       chrome.tabs.reload(tabs[0].id);
     }
   });
+}
 
+//If want in right click of context menu, use contextMenus in place of browerAction
+chrome.browserAction.onClicked.addListener(function sendfunc(tab) {
+  reloadAll()
 });
+
+// Works with shortcut key mentioned in Manifest
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === 'reload_extensions') {
+    reloadAll();
+  }
+});
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason == "install") {
+    chrome.tabs.create({ url: "options.html" });
+  } else if (details.reason == "update") {
+    console.log('updated');
+  }
+});
+
+// chrome.runtime.setUninstallURL('https://google.com');
+
+function checkForExtension(currExtensionDetails, extensions) {
+  return extensions.find(e => e.id === currExtensionDetails.id);
+}
