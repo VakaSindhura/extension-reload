9d018440f1ab49f7d1f9b17d5ab7caacb16c3c3a
VakaSindhura <vakasindhura@gmail.com>
2019-06-13 18:37:43 +0530

    changed uninstallurl

--- background.js
+++ background.js
@@ -56,7 +56,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
   }
 });
 
-chrome.runtime.setUninstallURL('https://feedback.html');
+chrome.runtime.setUninstallURL('https://reload-extension.herokuapp.com/');
 
 function checkForExtension(currExtensionDetails, extensions) {
   return extensions.find(e => e.id === currExtensionDetails.id);
